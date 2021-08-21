9076b58db27e989a0c0408bd8bec59e2342be123
j108251113 <j108251113@nkust.edu.tw>
2021-08-21 21:15:01 +0800

    initial Median of Two Sorted Arrays test

---
+++ Median_of_Two_Sorted_Arrays/test_Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from Median_of_Two_Sorted_Arrays import Solution
+
+
+class TestSolution(TestCase):
+    def setUp(self) -> None:
+        self.solution = Solution()
+
+    def test_find_median_sorted_arrays(self):
+        self.fail()
